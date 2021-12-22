e5d20639ea0cdd1bd217ea4087c1771ce7467f6a
edsonsreis <edsomreis@gmail.com>
2021-12-22 20:19:28 -0300

    Commit utilizando o Intellij

---
+++ 2- Dominando IDEs Java/Programas/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+public class PrimeiroPrograma {
+	public static void main(String args []) {
+		int a = 1;
+		int b = 2;
+		int result = a+b;
+		System.out.println(result);
+	}
+
+
+}
---
+++ 2- Dominando IDEs Java/Programas/src/br/com/dio/SegundoPrograma.java
@@ -0,0 +1,20 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class SegundoPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+
+/*        int a = 5;
+        int b = 4;
+        int result = a+b;
+        System.out.println(result);*/
+
+
+    }
+
+}
