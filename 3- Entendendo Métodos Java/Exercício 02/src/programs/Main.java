c23c12136c2e1cc8c2a92eef4511e3d13da90d63
edsonsreis <edsomreis@gmail.com>
2021-12-23 12:00:18 -0300

    Alteração no exercício 02 de Entendendo Métodos Java

--- 3- Entendendo Métodos Java/Exercício 02/src/programs/Main.java
+++ 3- Entendendo Métodos Java/Exercício 02/src/programs/Main.java
@@ -1,14 +1,37 @@
 package programs;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        double quadrado, retangulob, retanguloh;
+        double trapezio_maior, trapezio_menor, trapezio_altura;
+        float losango1, losango2;
+
         // Quadrilátero
         System.out.println("Exercício quadrilátero");
-        Quadrilatero.area(3);
-        Quadrilatero.area(5d,5d);
-        Quadrilatero.area(7,8,9);
-        Quadrilatero.area(5f,5f);
+        System.out.println("Digite o lado do quadrado: ");
+        quadrado = scan.nextDouble();
+        System.out.println("Digite a base do retângulo: ");
+        retangulob = scan.nextDouble();
+        System.out.println("Digite a altura do retãngulo: ");
+        retanguloh = scan.nextDouble();
+        System.out.println("Digite a base Maior do Trapezio: ");
+        trapezio_maior = scan.nextDouble();
+        System.out.println("Digite a base Menor do Trapezio: ");
+        trapezio_menor = scan.nextDouble();
+        System.out.println("Digite a altura do Trapezio: ");
+        trapezio_altura = scan.nextDouble();
+        System.out.println("Digite a diagonal 1 do losango: ");
+        losango1 = scan.nextFloat();
+        System.out.println("Digite a diagonal 2 do losango: ");
+        losango2 = scan.nextFloat();
+        Quadrilatero.area(quadrado);
+        Quadrilatero.area(retangulob,retanguloh);
+        Quadrilatero.area(trapezio_maior,trapezio_menor,trapezio_altura);
+        Quadrilatero.area(losango1,losango2);
 
     }
 }
