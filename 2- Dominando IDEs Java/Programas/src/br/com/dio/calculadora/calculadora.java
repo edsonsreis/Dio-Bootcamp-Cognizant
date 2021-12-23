49d6eb41fb1f29dbe3921d3ac8f635e97acb4e08
edsonsreis <edsomreis@gmail.com>
2021-12-22 21:02:13 -0300

    Alterações no Intellij

---
+++ 2- Dominando IDEs Java/Programas/src/br/com/dio/calculadora/calculadora.java
@@ -0,0 +1,46 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class calculadora {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a, b;
+
+        System.out.println("Digite o primeiro valor: ");
+        a = scan.nextInt();
+        System.out.println("Digite o primeiro valor: ");
+        b = scan.nextInt();
+
+        int somar = somar(a, b);
+        int subtrair = subtrair(a, b);
+        int multiplicar = multiplicar(a, b);
+        int dividir = dividir(a, b);
+
+        System.out.println("Somar: " + somar);
+        System.out.println("Subtrair: " + subtrair);
+        System.out.println("Multiplicar: " + multiplicar);
+        System.out.println("Dividir: " + dividir);
+
+    }
+
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtrair(int a, int b){
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b){
+        return a * b;
+    }
+
+    public static int dividir(int a, int b){
+        return a / b;
+    }
+
+
+
+}
